fa0b483edee578e2ee956706e93727ca9d0d02f9
congdanh1802 <160849729+congdanh1802@users.noreply.github.com>
2024-02-25 01:32:28 +0700

    Update practice_2.sql

--- practice_2.sql
+++ practice_2.sql
@@ -8,3 +8,21 @@ count(city) - count(distinct city)
 from station;
 
 --ex3: hackerrank-the-blunder
+select 
+ceiling(avg(salary) - avg(replace(salary, '0', '')))
+from employees;
+
+--ex4: datalemur-alibaba-compressed-mean
+select round(cast(sum(item_count*order_occurrences)/sum(order_occurrences) as decimal),1) 
+from items_per_order;
+
+--ex5: datalemur-matching-skills
+SELECT
+candidate_id
+FROM candidates
+where skill in ('Python', 'Tableau', 'PostgreSQL')
+group by candidate_id
+having count(skill) = 3
+order by candidate_id;
+
+--ex6: datalemur-verage-post-hiatus-1
