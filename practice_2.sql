7243784e223588aee54899c9cb402fa50c76f295
congdanh1802 <160849729+congdanh1802@users.noreply.github.com>
2024-02-25 13:48:06 +0700

    Update practice_2.sql

--- practice_2.sql
+++ practice_2.sql
@@ -26,3 +26,47 @@ having count(skill) = 3
 order by candidate_id;
 
 --ex6: datalemur-verage-post-hiatus-1
+SELECT user_id,
+max(date(post_date)) - min(date(post_date))
+FROM posts
+where post_date between '01/01/2021' and '01/01/2022'
+group by user_id
+having count(post_date) >= 2;
+
+--ex7: datalemur-cards-issued-difference
+SELECT card_name,
+max(issued_amount) - min(issued_amount) as disparity
+FROM monthly_cards_issued
+group by card_name
+order by disparity desc;
+
+--ex8: datalemur-non-profitable-drugs
+SELECT manufacturer, count(drug) as count_drug,
+sum(abs(total_sales-cogs)) as losses
+FROM pharmacy_sales
+where cogs>total_sales
+group by manufacturer
+order by losses desc;
+
+--ex9: leetcode-not-boring-movies
+select *
+from cinema
+where id%2 = 1 and description not like 'boring'
+order by rating desc;
+
+--ex10: leetcode-number-of-unique-subject
+select teacher_id, count(distinct subject_id) as cnt
+from teacher
+group by teacher_id;
+
+--ex11: leetcode-find-followers-count
+select user_id, count(follower_id) as followers_count
+from followers
+group by user_id;
+
+--ex12:leetcode-classes-more-than-5-students
+select class
+from courses 
+group by class
+having count(distinct student) >= 5;
+
